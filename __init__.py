3276842742fa33548879dbf7b6897c2de64e7164
Shubham Sharma <shubham.sharma3297@gmail.com>
2024-12-15 21:50:58 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,52 +1,24 @@
-from collections import namedtuple
+import subprocess
+from pathlib import Path
 
-import numpy as np
-import torch
+from .version import __version__
 
-from .detectors import build_detector
-
-try:
-    import kornia
-except:
-    pass
-    # print('Warning: kornia is not installed. This package is only required by CaDDN')
+__all__ = [
+    '__version__'
+]
 
 
+def get_git_commit_number():
+    if not (Path(__file__).parent / '../.git').exists():
+        return '0000000'
 
-def build_network(model_cfg, num_class, dataset):
-    model = build_detector(
-        model_cfg=model_cfg, num_class=num_class, dataset=dataset
-    )
-    return model
-
-
-def load_data_to_gpu(batch_dict):
-    for key, val in batch_dict.items():
-        if not isinstance(val, np.ndarray):
-            continue
-        elif key in ['frame_id', 'metadata', 'calib', 'image_shape', 'sample_idx']:
-            continue
-        elif key in ['images']:
-            batch_dict[key] = kornia.image_to_tensor(val).float().cuda().contiguous()
-        elif key in ['image_shape']:
-            batch_dict[key] = torch.from_numpy(val).int().cuda()
-        else:
-            batch_dict[key] = torch.from_numpy(val).float().cuda()
+    cmd_out = subprocess.run(['git', 'rev-parse', 'HEAD'], stdout=subprocess.PIPE)
+    git_commit_number = cmd_out.stdout.decode('utf-8')[:7]
+    return git_commit_number
 
 
-def model_fn_decorator():
-    ModelReturn = namedtuple('ModelReturn', ['loss', 'tb_dict', 'disp_dict'])
+script_version = get_git_commit_number()
 
-    def model_func(model, batch_dict):
-        load_data_to_gpu(batch_dict)
-        ret_dict, tb_dict, disp_dict = model(batch_dict)
 
-        loss = ret_dict['loss'].mean()
-        if hasattr(model, 'update_global_step'):
-            model.update_global_step()
-        else:
-            model.module.update_global_step()
-
-        return ModelReturn(loss, tb_dict, disp_dict)
-
-    return model_func
+if script_version not in __version__:
+    __version__ = __version__ + '+py%s' % script_version
